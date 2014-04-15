921a877965015b6d1a08fefe82d67e765eb53f9f
Christoph Boehner <cb@vorwaerts-werbung.de>
2014-04-15 19:29:54 +0200

    Add newitem listing to frontpage

--- src/klikev.sitecontent/klikev/sitecontent/frontpage.py
+++ src/klikev.sitecontent/klikev/sitecontent/frontpage.py
@@ -1,4 +1,7 @@
 from five import grok
+from plone import api
+
+from plone.app.contentlisting.interfaces import IContentListing
 from plone.app.layout.navigation.interfaces import INavigationRoot
 
 
@@ -6,3 +9,15 @@ class FrontpageView(grok.View):
     grok.context(INavigationRoot)
     grok.require('zope2.View')
     grok.name('frontpage-view')
+
+    def update(self):
+        self.has_newsitems = len(self.newsitems()) > 0
+
+    def newsitems(self):
+        catalog = api.portal.get_tool(name="portal_catalog")
+        items = catalog(portal_type='plone.app.contenttypes.content.NewsItem',
+                        review_state='published',
+                        sort_on='effective',
+                        sort_order='reverse')
+        results = IContentListing(items)
+        return results
