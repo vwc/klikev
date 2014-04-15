4a30c46ffeb0f4091a5cbf89fb9ba58b5aca2764
Christoph Boehner <cb@vorwaerts-werbung.de>
2014-04-15 20:50:43 +0200

    Update frontpage view and rebuild theme

--- src/klikev.sitecontent/klikev/sitecontent/frontpage.py
+++ src/klikev.sitecontent/klikev/sitecontent/frontpage.py
@@ -4,6 +4,8 @@ from plone import api
 from plone.app.contentlisting.interfaces import IContentListing
 from plone.app.layout.navigation.interfaces import INavigationRoot
 
+from plone.app.contenttypes.interfaces import INewsItem
+
 
 class FrontpageView(grok.View):
     grok.context(INavigationRoot)
@@ -15,9 +17,10 @@ class FrontpageView(grok.View):
 
     def newsitems(self):
         catalog = api.portal.get_tool(name="portal_catalog")
-        items = catalog(portal_type='plone.app.contenttypes.content.NewsItem',
+        items = catalog(object_provides=INewsItem.__identifier__,
                         review_state='published',
                         sort_on='effective',
-                        sort_order='reverse')
+                        sort_order='reverse',
+                        sort_limit=3)[:3]
         results = IContentListing(items)
         return results
